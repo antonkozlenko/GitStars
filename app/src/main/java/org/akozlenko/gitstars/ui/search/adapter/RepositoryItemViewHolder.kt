038f2147e93c5ef17cf076745fd485202e99be72
Anton Kozlenko <anton.kozlenko90@gmail.com>
2023-01-27 20:31:28 +0200

    Added missing network config for Charles request debugging

--- app/src/main/java/org/akozlenko/gitstars/ui/search/adapter/RepositoryItemViewHolder.kt
+++ app/src/main/java/org/akozlenko/gitstars/ui/search/adapter/RepositoryItemViewHolder.kt
@@ -2,7 +2,6 @@ package org.akozlenko.gitstars.ui.search.adapter
 
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
